3bf67cb1a5aa5028e9803685227fdbc78619c440
israel palma <ipalm001@gmail.com>
2021-09-14 21:58:36 -0300

    ejercicios Dojo Read

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from login.models import User
+
+
+# Create your views here.
+def home(request):
+    reg_user = User.objects.get(id=request.session['user_id'])
+
+    context = {
+        "active_user": reg_user,
+    }
+
+    return render(request, 'home.html', context)
+
+def agregar(request):
+    return render(request, 'agregar.html')
